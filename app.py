7bf8836e2b67924a17d681d708d027ed249389ba
Mahdi shahnavaz <mahdiprog20@gmail.com>
2024-04-08 20:23:45 +0430

    init priject

---
+++ app.py
@@ -0,0 +1,33 @@
+print("***Welcome to my enctypt machine***")
+encrypted_text = ""
+decoded_text = ""
+
+while True:
+    inp = int(input("Enter your choice number: \n\t1-Encript \n\t2-Decode \n\t3-Exit\n Enter: "))
+    if(inp == 1):
+        encrypt_inp = input("Enter your text: ")
+        for i in range(0,len(encrypt_inp)):
+            aski_num = ord(encrypt_inp[i])
+            encrypted_text += chr(aski_num *6 +2)   
+        print(f"Your encrypted text is: {encrypted_text}\n",end="")
+        inp1 = input("Do you want to continue: y/n  ").lower()
+        if(inp1 == 'y'):
+            continue
+        elif(inp1 == 'n'):
+            break    
+    elif(inp == 2):
+        decode_text = input("Enter your text: ")
+        for i in range(0,len(decode_text)):
+            aski_num = ord(decode_text[i])
+            decoded_num = (aski_num-2)//6
+            decoded_text += chr(decoded_num)   
+        print(f"Your decodeded text is: {decoded_text}\n",end="")
+        inp2 = input("Do you want to continue: y/n  ").lower()
+        if(inp2 == 'y'):
+            continue
+        elif(inp2 == 'n'):
+            break  
+    elif(inp == 3):
+        break 
+    else:
+        print("your entry isn't correct, please enter a valid number :)")       
